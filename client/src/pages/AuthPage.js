bb7322c31638ec5bc25e04ad4bd0916edb735d6d
Яров Михаил <vbif.misha2012@yandex.ru>
2020-12-10 00:23:33 +0300

    added dark theme to all pages

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import {useTheme} from "../context/ThemeContext";
+import classNames from "classnames";
 
 export const AuthPage = () => {
+    const theme = useTheme()
+
+    const styleBackground = classNames({
+        "dark" : theme.darkMode
+    })
+
     return (
-        <h1>Auth page</h1>
+        <div className={styleBackground}>
+            <h1>Auth page</h1>
+        </div>
     )
 }
 
--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import {useTheme} from "../context/ThemeContext";
+import classNames from "classnames";
 
 export const ProfilePage = () => {
+    const theme = useTheme()
+
+    const styleBackground = classNames({
+        "dark" : theme.darkMode
+    })
+
     return (
-        <h1>Profile page</h1>
+        <div className={styleBackground}>
+            <h1>Profile page</h1>
+        </div>
     )
 }
 
