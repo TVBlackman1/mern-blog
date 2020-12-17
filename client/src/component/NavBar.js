9deebfe585255ad3c805b72811a7d31f2849dbd9
Яров Михаил <vbif.misha2012@yandex.ru>
2020-12-17 21:34:15 +0300

    start redesign our header. Main page next

--- client/src/component/NavBar.js
+++ client/src/component/NavBar.js
@@ -3,12 +3,16 @@ import { NavLink } from 'react-router-dom'
 import classNames from 'classnames'
 import {ThemeContext, themes } from '../context/ThemeContext'
 import {useAuthRequest} from "../hooks/useAuthRequest";
+import "../styles/nav.css"
 
 
 export const NavBar = () => {
     const { logout } = useAuthRequest()
 
     const renderComponent = ({ currentTheme, toggleTheme }) => {
+        const styleTheme = classNames({
+            [currentTheme]: true
+        })
         const styleNav = classNames({
             "blue lighten-5" : currentTheme === themes.light,
             "grey darken-2" : currentTheme === themes.dark,
@@ -17,17 +21,28 @@ export const NavBar = () => {
 
 
         return (
-            <nav className={styleNav}>
-                <div className="nav-wrapper">
-                    <ul id="nav-mobile" className="left hide-on-med-and-down">
-                        <li><NavLink to={"/main"}>Main</NavLink></li>
-                        <li><NavLink to={"/profile"}>Profile</NavLink></li>
-                        <li><NavLink to={"/auth"}>Auth</NavLink></li>
-                        <li><button className="waves-effect waves-light btn-small" onClick={toggleTheme}>Theme</button></li>
-                        <li><button className="waves-effect waves-light btn-small" onClick={logout}>Выйти</button></li>
+            // <ul className="nav nav-pills">
+            //     <li className="nav-item"><NavLink to={"/main"}>Main</NavLink></li>
+            //     <li className="nav-item"><NavLink to={"/profile"}>Profile</NavLink></li>
+            //     <li className="nav-item"><NavLink to={"/auth"}>Auth</NavLink></li>
+            //     <li className="nav-item"><button className="waves-effect waves-light btn-small" onClick={toggleTheme}>Theme</button></li>
+            //     <li className="nav-item"><button className="waves-effect waves-light btn-small" onClick={logout}>Выйти</button></li>
+            // </ul>
+            <header>
+                <nav>
+                    <ul className="left">
+                        <li><NavLink style={{ textDecoration: 'none' }} to={"/main"}><p>Главная</p></NavLink></li>
+                        <li><NavLink style={{ textDecoration: 'none' }} to={"/profile"}><p>Профиль</p></NavLink></li>
+                        <li><NavLink style={{ textDecoration: 'none' }} to={"/recommended"}><p>Рекомендации</p></NavLink></li>
+                    </ul>
+                </nav>
+                <div className="right">
+                    <ul className="right">
+                        <li><button>Тема</button></li>
+                        <li><NavLink style={{ textDecoration: 'none' }} to={"/auth"}><p>Войти</p></NavLink></li>
                     </ul>
                 </div>
-            </nav>
+            </header>
         )
     }
 
